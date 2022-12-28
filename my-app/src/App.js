3e89c68fba9afc8a7070932fca968f3ff63cf6dc
Rakesh halijol <rakeshhalijol@gmail.com>
2022-12-28 20:56:23 +0530

    Intial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Whether from './components/Whether';
+
+function App() {
+  return (
+    <div className="App">
+      <Whether/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Whether.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Whether() {
+  return (
+    <div>
+      <h2>Whether app under construction...</h2>
+    </div>
+  )
+}
